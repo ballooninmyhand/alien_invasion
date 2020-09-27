be5dc10a758112274e91b47221266168218583ed
luoding <18852951201@163.com>
2020-09-27 13:51:43 +0800

    攻击外星人 & 开始重置游戏

--- alien.py
+++ alien.py
@@ -1,8 +1,10 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
     """表示单个外星人的类"""
+
     def __init__(self, ai_settings, screen):
         """初始化外星人并设置其起始位置"""
         super(Alien, self).__init__()
@@ -20,3 +22,14 @@ class Alien(Sprite):
     def blit_me(self):
         """在指定位置绘制外星人"""
         self.screen.blit(self.image, self.rect)
+
+    def check_edges(self):
+        """如果外星人位于屏幕边缘，就返回 True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """向右移动外星人"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
